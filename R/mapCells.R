a7ed1e64993c7280f9600338b5cb636eb688a14e
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-01 08:59:58 -0400

    Prepare mapCells

---
+++ R/mapCells.R
@@ -0,0 +1,12 @@
+#' Map cell names to identifiers using Cellosaurus
+#'
+#' @export
+#' @note Updated 2020-08-01.
+#'
+#' @param x `character`.
+#'   Cell names or Cellosaurus identifiers.
+#'
+#' @examples
+#' mapCells("22RV1")
+mapCells <- function(x) {
+}
