b819283827088a937a6d910d9d97920427f8df57
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-02 13:59:53 -0400

    Initial working version

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,7 @@
 #' @importFrom cli cli_alert_warning
 #' @importFrom goalie assert hasRows isCharacter isString
 #' @importFrom httr GET content
-#' @importFrom stringr str_match str_split
+#' @importFrom stringr str_match str_replace str_replace_all str_split
 #' @importFrom syntactic snakeCase
 #' @importFrom utils URLencode
 NULL
