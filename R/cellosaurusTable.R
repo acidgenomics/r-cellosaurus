df5fc714cfe7f9280ec55f7ac91a185e575b5764
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-02 15:48:11 -0400

    Draft update to cellosaurus table

--- R/cellosaurusTable.R
+++ R/cellosaurusTable.R
@@ -23,66 +23,93 @@ cellosaurusTable <- function(
             response <- GET(url)
             content <- content(response, as = "text")
             lines <- strsplit(content, split = "\n")[[1L]]
-            ## Disease: NCIt
-            ncit <- .str_subset_and_match(
-                string = lines,
-                pattern = "^DI   NCIt; (C[0-9]+); (.+)$"
+            ## Cell line name.
+            cellLineName <- .str_subset_and_match_single(
+                string = head(lines, n = 1L),
+                pattern = "^<pre>ID[[:space:]]+(.+)$"
             )
-            if (identical(nrow(ncit), 1L)) {
-                ncitDiseaseID <- ncit[1L, 2L]
-                ncitDiseaseName <- ncit[1L, 3L]
+            assert(isString(cellLineName))
+            ## Disease: NCIt.
+            match <- .str_subset_and_match(
+                string = lines,
+                pattern = "^DI[[:space:]]+NCIt; (C[0-9]+); (.+)$"
+            )
+            if (hasRows(match)) {
+                ncitDiseaseID <- match[1L, 2L]
+                ncitDiseaseName <- match[1L, 3L]
             } else {
                 ncitDiseaseID <- NULL
                 ncitDiseaseName <- NULL
             }
-
-            tibble(
-                cellosaurus_id = id,
-                ncitDiseaseID = ncitDiseaseID,
-                ncitDiseaseName = ncitDiseaseName
+            ## Organism.
+            organism <- .str_subset_and_match_single(
+                string = lines,
+                pattern = "^OX.+! (.+)$"
             )
-
-
-            ## Useful stuff:
-            ## Disease 	Bladder carcinoma (NCIt: C4912)
-            ## Species of origin 	Homo sapiens (Human) (NCBI Taxonomy: 9606)
-            ## Sex of cell 	Male
-            ## Age at sampling 	68Y
-            ## Category 	Cancer cell line
-            ##
-            ## Cell line collections 	AddexBio; C0002001/8
-            ##ATCC; HTB-9
-            ##BCRC; 60061
-            ##BCRJ; 0026
-            ##CLS; 300105/p667_5637
-            ##DSMZ; ACC-35
-            ##ICLC; HTL01017
-            ##KCLB; 30009
-            ##NCBI_Iran; C450
-            ##RCB; RCB1191
-            ##RCB; RCB3676
-            ##TKG; TKG 0605
-            ##Cell line databases/resources 	CLDB; cl1
-            ##CLDB; cl105
-            ##CLDB; cl7118
-            ##CCLE; 5637_URINARY_TRACT
-            ##CCRID; 3111C0001CCC000242
-            ##CCRID; 3131C0001000700001
-            ##Cell_Model_Passport; SIDM00807
-            ##Cosmic-CLP; 687452
-            ##DepMap; ACH-000905
-            ##IGRhCellID; 5637
-            ##LINCS_HMS; 50001
-            ##LINCS_LDP; LCL-1702
-            ##SKY/M-FISH/CGH; 17
-            ##
-            ##Ontologies 	BTO; BTO:0003137
-            ##CLO; CLO_0001421
-            ##CLO; CLO_0050845
-            ##EFO; EFO_0002096
-            ##MCCL; MCC:0000018
-            ##
-            ## Wikidata; Q54603866
+            ## Patient age.
+            patientAgeYears <- .str_subset_and_match_single(
+                string = lines,
+                pattern = "^AG[[:space:]]+([0-9]+)Y$"
+            )
+            ## Patient sex.
+            patientSex <- .str_subset_and_match_single(
+                string = lines,
+                pattern = "^SX[[:space:]]+(.+)$"
+            )
+            ## Is the cell line a cancer cell?
+            isCancer <- any(str_detect(
+                string = lines,
+                pattern = "^CA[[:space:]]+Cancer cell line$"
+            ))
+            ## Prepare tibble.
+            out <- tibble(
+                cellLineName = cellLineName,
+                cellosaurusID = id,
+                ncitDiseaseID = ncitDiseaseID,
+                ncitDiseaseName = ncitDiseaseName,
+                patientAgeYears = patientAgeYears,
+                patientSex = patientSex,
+                isCancer = isCancer
+            )
+            ## Filter entries with "DR" tag.
+            dr <- str_match(
+                string = lines,
+                pattern = "^DR[[:space:]]+(.+); (.+)$"
+            )
+            dr <- dr[complete.cases(dr), c(2L, 3L), drop = FALSE]
+            colnames(dr) <- c("key", "value")
+            dr <- as.data.frame(dr)
+            keep <- dr[[1L]] %in% c(
+                ## Cell line databases / resource.
+                "CCLE",
+                "Cell_Model_Passport",
+                "Cosmic-CLP",
+                "DepMap",
+                "IGRhCellID",
+                "LINCS_HMS",
+                "LINCS_LDP",
+                ## Ontologies:
+                "BTO",
+                "CLO",
+                "EFO",
+                "MCCL"
+            )
+            dr <- dr[keep, , drop = FALSE]
+            dr <- arrange_all(dr)
+            dr <- group_by(dr, !!sym("key"))
+            dr <- summarize(
+                dr,
+                value = toString(!!sym("value")),
+                .groups = "keep"
+            )
+            dr <- ungroup(dr)
+            dr <- as.data.frame(dr)
+            dr <- column_to_rownames(dr, "key")
+            dr <- t(dr)
+            rownames(dr) <- NULL
+            out <- cbind(out, dr)
+            out <- camelCase(out)
+            out
         },
         BPPARAM = BPPARAM
     )
