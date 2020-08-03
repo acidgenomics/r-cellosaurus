524ee75be3787e88d828c8efa27f3acbce00f5ff
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-03 08:32:59 -0400

    Update documentation

--- R/standardizeCells.R
+++ R/standardizeCells.R
@@ -2,6 +2,15 @@
 #'
 #' @export
 #' @note Updated 2020-08-02.
+#'
+#' @details
+#' Strip all non-alphanumeric characters, remove information in
+#' parentheses/bracket, and convert to uppercase.
+#'
+#' @return `character`.
+#'
+#' @examples
+#' standardizeCells(c("22Rv1", "Jurkat", "Ramos (RA-1)"))
 standardizeCells <- function(x) {
     assert(isCharacter(x))
     x <- tolower(x)
