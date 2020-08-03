e01d145ecc1982696735ca1ba4cf6858e7ffa7c6
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-03 08:47:35 -0400

    Prepare initial package release

--- R/imports.R
+++ R/imports.R
@@ -1,11 +1,16 @@
+#' @importMethodsFrom S4Vectors coerce
 #' @importFrom BiocParallel bplapply bpparam
 #' @importFrom cli cli_alert_warning
+#' @importFrom data.table rbindlist
+#' @importFrom dplyr arrange_all group_by summarize ungroup
 #' @importFrom goalie allAreMatchingFixed assert hasRows isCharacter isString
 #' @importFrom httr GET content
+#' @importFrom methods as
 #' @importFrom rlang !! sym
+#' @importFrom stats complete.cases
 #' @importFrom stringr str_detect str_match str_replace str_replace_all
 #'   str_split str_subset
 #' @importFrom syntactic camelCase snakeCase
 #' @importFrom tibble column_to_rownames tibble
-#' @importFrom utils URLencode
+#' @importFrom utils URLencode head
 NULL
