7ceaa5c69495b2c8f1317e60236f0d82064e5835
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-01 09:27:55 -0400

    Work toward 100% coverage

--- tests/testthat/test-CellosaurusTable.R
+++ tests/testthat/test-CellosaurusTable.R
@@ -2,6 +2,6 @@ context("CellosaurusTable")
 
 test_that("CellosaurusTable", {
     ids <- c("CVCL_0126", "CVCL_1045")
-    object <- CellosaurusTable(ids)
+    object <- CellosaurusTable(x = ids, BPPARAM = BPPARAM)
     expect_s4_class(object, "CellosaurusTable")
 })
