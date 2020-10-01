9ff7e3d1bc4b387ba33b88f035f3105e930b2fea
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-01 08:55:09 -0400

    Deprecate cellosaurusTable in favor of CellosaurusTable (note case)

---
+++ R/deprecated.R
@@ -0,0 +1,20 @@
+## nocov start
+## nolint start
+
+
+
+#' @name deprecated
+#' @inherit acidroxygen::deprecated description examples return seealso title
+#' @inheritParams acidroxygen::params
+#' @keywords internal
+NULL
+
+
+
+## v0.0.1 ======================================================================
+#' @rdname deprecated
+#' @export
+cellosaurusTable <- function(...) {
+    .Deprecated("CellosaurusTable")
+    CellosaurusTable(...)
+}
