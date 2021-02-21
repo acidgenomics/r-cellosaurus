872515aab288d79bbf556cde5aa34e5d87ce08da
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-21 12:54:42 -0500

    Work toward stable release

--- tests/testthat/test-mapCells.R
+++ tests/testthat/test-mapCells.R
@@ -2,7 +2,7 @@ context("mapCells")
 
 test_that("Cell name", {
     cells <- c("22RV1", "HS729", "JURKAT")
-    object <- mapCells(cells, BPPARAM = BPPARAM)
+    object <- mapCells(object = cells)
     expected <- c(
         "22RV1" = "CVCL_1045",
         "HS729" = "CVCL_0871",
@@ -12,7 +12,7 @@ test_that("Cell name", {
 })
 
 test_that("Match failure", {
-    object <- mapCells(c("XXXXXX", "YYYYYY"), BPPARAM = BPPARAM)
+    object <- mapCells(c("XXXXXX", "YYYYYY"))
     expected <- c("XXXXXX" = NA_character_, "YYYYYY" = NA_character_)
     expect_identical(object, expected)
 })
