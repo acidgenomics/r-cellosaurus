0456bb2846aa115c75fd04a34ec63fe8e6893048
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-21 12:47:12 -0500

    Rework cellosaurus generator

--- R/AllGenerators.R
+++ R/AllGenerators.R
@@ -1,7 +1,7 @@
 #' Cellosaurus table of cell identifier mappings
 #'
 #' @export
-#' @note Updated 2021-02-19.
+#' @note Updated 2021-02-21.
 #'
 #' @param x `character`.
 #'   Cellosaurus identifiers (e.g. `CVCL_*`.)
@@ -30,52 +30,57 @@ CellosaurusTable <-  # nolint
                 response <- GET(url)
                 content <- content(response, as = "text")
                 lines <- strsplit(content, split = "\n")[[1L]]
-                ## Cell line name.
-                cellLineName <- .strSubsetAndMatchSingle(
-                    string = head(lines, n = 1L),
-                    pattern = "^<pre>ID[[:space:]]+(.+)$"
+                out <- list(
+                    "cellosaurusId" = id,
+                    "cellLineName" = .strSubsetAndMatchSingle(
+                        string = head(lines, n = 1L),
+                        pattern = "^<pre>ID[[:space:]]+(.+)$"
+                    ),
+                    "isCancer" = any(str_detect(
+                        string = lines,
+                        pattern = "^CA[[:space:]]+Cancer cell line$"
+                    )),
+                    "isProblematic" = any(str_detect(
+                        string = lines,
+                        pattern = "^CC[[:space:]]+Problematic cell line.*$"
+                    )),
+                    "organism" = .strSubsetAndMatchSingle(
+                        string = lines,
+                        pattern = "^OX.+! (.+)$"
+                    ),
+                    "patientAgeYears" = as.integer(.strSubsetAndMatchSingle(
+                        string = lines,
+                        pattern = "^AG[[:space:]]+([0-9]+)Y$"
+                    )),
+                    "patientSex" = .strSubsetAndMatchSingle(
+                        string = lines,
+                        pattern = "^SX[[:space:]]+(.+)$"
+                    )
                 )
-                assert(isString(cellLineName))
-                ## Is the cell line problematic?
-                isProblematic <- any(str_detect(
-                    string = lines,
-                    pattern = "^CC[[:space:]]+Problematic cell line.*$"
-                ))
+                assert(
+                    isString(out[["cellLineName"]]),
+                    isFlag(out[["isCancer"]]),
+                    isFlag(out[["isProblematic"]]),
+                    isOrganism(out[["organism"]]),
+                    isInt(out[["patientAgeYears"]]) ||
+                        is.na(out[["patientAgeYears"]]),
+                    isSubset(out[["patientSex"]], c("Female", "Male"))
+                )
                 ## Disease: NCIt.
                 ## Note that we're censoring problematic cell lines here.
                 match <- .strSubsetAndMatch(
                     string = lines,
                     pattern = "^DI[[:space:]]+NCIt; (C[0-9]+); (.+)$"
                 )
-                if (hasRows(match) && !isTRUE(isProblematic)) {
-                    ncitDiseaseId <- match[1L, 2L]
-                    ncitDiseaseName <- match[1L, 3L]
+                if (hasRows(match) && !isTRUE(out[["isProblematic"]])) {
+                    out[["ncitDiseaseId"]] <- match[1L, 2L]
+                    out[["ncitDiseaseName"]] <- match[1L, 3L]
                 } else {
                     ## nocov start
-                    ncitDiseaseId <- NA
-                    ncitDiseaseName <- NA
+                    ncitDiseaseId <- NA_character_
+                    ncitDiseaseName <- NA_character_
                     ## nocov end
                 }
-                ## Organism.
-                organism <- .strSubsetAndMatchSingle(
-                    string = lines,
-                    pattern = "^OX.+! (.+)$"
-                )
-                ## Patient age.
-                patientAgeYears <- .strSubsetAndMatchSingle(
-                    string = lines,
-                    pattern = "^AG[[:space:]]+([0-9]+)Y$"
-                )
-                ## Patient sex.
-                patientSex <- .strSubsetAndMatchSingle(
-                    string = lines,
-                    pattern = "^SX[[:space:]]+(.+)$"
-                )
-                ## Is the cell line a cancer cell?
-                isCancer <- any(str_detect(
-                    string = lines,
-                    pattern = "^CA[[:space:]]+Cancer cell line$"
-                ))
                 ## Filter entries with "DR" tag.
                 dr <- str_match(
                     string = lines,
@@ -107,37 +112,17 @@ CellosaurusTable <-  # nolint
                 assert(is.data.frame(dr))
                 dr2 <- dr[["value"]]
                 names(dr2) <- dr[["key"]]
-                out <- list(
-                    "cellLineName" = cellLineName,
-                    "cellosaurusId" = id,
-                    "isCancer" = isCancer,
-                    "isProblematic" = isProblematic,
-                    "ncitDiseaseId" = ncitDiseaseId,
-                    "ncitDiseaseName" = ncitDiseaseName,
-                    "patientSex" = patientSex,
-                    "patientAgeYears" = patientAgeYears
-                )
                 out <- append(x = out, values = dr2)
                 names(out) <- camelCase(names(out), strict = TRUE)
                 out
             }
         )
         names(list) <- x
-        ## Alternatively, `rbindlist(l = list, fill = TRUE)` is a useful
-        ## approach to consider, but it doesn't return 1:1 on the rows.
-
-        ## FIXME Need to rework `as.DataFrame()` to handle this??
-
-        ## FIXME THIS IS CREATING DUPLICATE LINES ARGH....
-        ## FIXME THIS FUNCTION IS MESSED UP AND NEEDS A REWORK IN PIPETTE...
-        ## FIXME rbindlist is also doing this too, what's up with that.
-        ## FIXME ITS BECAUSE CLO HAS DUPLICATES HERE...NEED TO USE I() HERE...
-        xxx <- unlistToDataFrame(list)
-        ## FIXME CREATE DATA FRAME AT LAST STEP...
-        ## FIXME Can we switch to `unlistToDataFrame` here?
-
-
-        data <- as(data, "DataFrame")
-        rownames(data) <- data[["cellosaurusId"]]
-        new("CellosaurusTable", data)
+        df <- rbindToDataFrame(list)
+        df <- df[, sort(colnames(df)), drop = FALSE]
+        assert(
+            is(df, "DataFrame"),
+            identical(rownames(df), x)
+        )
+        new("CellosaurusTable", df)
     }
--- R/package.R
+++ R/package.R
@@ -9,9 +9,9 @@
 #' @importMethodsFrom basejump coerce
 #'
 #' @importFrom basejump DataFrame aggregate alertWarning append camelCase
-#'   complete.cases head import pasteURL rbindlist snakeCase unlistToDataFrame
+#'   complete.cases head import pasteURL rbindToDataFrame snakeCase
 #' @importFrom goalie allAreMatchingFixed assert hasLength hasRownames hasRows
-#'   isCharacter isString isSubset validate
+#'   isCharacter isFlag isInt isOrganism isString isSubset validate
 #' @importFrom httr GET content
 #' @importFrom methods as new setClass setMethod setValidity
 #' @importFrom stringr str_detect str_match str_replace str_replace_all
