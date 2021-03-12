29f5fdd5a6f35d37dc53dd80316cb55ae258a733
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-12 11:07:42 -0500

    Documentation update

--- R/package.R
+++ R/package.R
@@ -10,11 +10,10 @@
 #'
 #' @importFrom basejump DataFrame URLencode aggregate alertWarning append
 #'   camelCase complete.cases formula head import packageName pasteURL
-#'   rbindToDataFrame snakeCase
+#'   rbindToDataFrame snakeCase str_detect str_match str_replace str_replace_all
+#'   str_split str_subset
 #' @importFrom goalie allAreMatchingFixed assert hasLength hasRownames hasRows
 #'   isCharacter isFlag isInt isOrganism isString isSubset validate
 #' @importFrom httr GET content
 #' @importFrom methods as is new setClass setMethod setValidity
-#' @importFrom stringr str_detect str_match str_replace str_replace_all
-#'   str_split str_subset
 "_PACKAGE"
