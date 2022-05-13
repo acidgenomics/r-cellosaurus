e6b804955e90946f7978f193eed7b06a0ab74eb3
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-13 08:20:59 -0400

    Improve main Cellosaurus object generation

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,6 +1,6 @@
 #' Cellosaurus table of cell identifier mappings
 #'
-#' @note Updated 2022-05-12.
+#' @note Updated 2022-05-13.
 #' @export
 #'
 #' @return `Cellosaurus`.
@@ -13,14 +13,37 @@ setValidity(
     method = function(object) {
         validate(
             hasRownames(object),
-            isSubset(
-                x = c("cellLineName", "cellosaurusId"),
-                y = colnames(object)
-            ),
             allAreMatchingFixed(
-                x = object[["cellosaurusId"]],
+                x = rownames(object),
                 pattern = "CVCL_"
             )
+        )
+        validateClasses(
+            object = object,
+            expected = c(
+                "ancestors" = "character",
+                "comment" = "CompressedCharacterList",
+                "creationDate" = "character",
+                "depMapId" = "character",
+                "derivedFrom" = "character",
+                "id" = "character",
+                "isCancer" = "logical",
+                "isProblematic" = "logical",
+                "isSymmetric" = "character",
+                "isTransitive" = "character",
+                "name" = "character",
+                "ncbiTaxonomyId" = "Rle",
+                "ncitDiseaseId" = "Rle",
+                "ncitDiseaseName" = "Rle",
+                "obsolete" = "logical",
+                "organism" = "Rle",
+                "originateFromSameIndividualAs" = "CompressedCharacterList",
+                "sangerId" = "character",
+                "subset" = "CompressedCharacterList",
+                "synonym" = "CompressedCharacterList",
+                "xref" = "CompressedCharacterList"
+            ),
+            subset = FALSE
         )
     }
 )
