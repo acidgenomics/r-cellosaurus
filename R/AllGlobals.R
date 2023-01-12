c5933fb1829072a84007152bdc708d371e08b679
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-12 08:28:43 -0500

    Store the latest supported release version internally

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -1,1 +1,12 @@
 .pkgName <- packageName()
+
+
+
+#' Cellosaurus release version
+#'
+#' @note Updated 2023-01-12.
+#' @noRd
+#'
+#' @details
+#' Note that ontologyIndex currently has parsing problems with 44 release.
+.release <- 43L
