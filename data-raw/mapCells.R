af406f6a60d5e0224b6b53f4b668b4a1edc4ad8f
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-17 14:56:11 -0500

    Draft updates

--- data-raw/mapCells.R
+++ data-raw/mapCells.R
@@ -21,16 +21,9 @@ df <- df[!is.na(df[["CellLineName"]]), ]
 df <- df[!duplicated(df[["CellLineName"]]), ]
 map[["depmapFail"]] <- df[is.na(df[["RRID"]]), "CellLineName"]
 df <- df[!is.na(df[["RRID"]]), ]
-## Remove cell lines that map ambiguously.
+## Remove cell lines that map incorrectly to Cellosaurus.
 censor <- c(
-    "ACH_000058", # ML-1
-    "ACH_001347", # H157
-    "ACH_001743", # RC2
-    "ACH_002099", # CS1
-    "ACH_002147", # K2
-    "ACH_002185", # PL18
-    "ACH_002325", # LY2
-    "ACH_002475" # HAP1
+    "ACH_002185" # PL18
 )
 df <- df[setdiff(rownames(df), censor), ]
 map[["depmap"]] <- df
@@ -53,25 +46,10 @@ df <- df[!duplicated(df[["model_name"]]), ]
 map[["cmpFail"]] <- df[is.na(df[["RRID"]]), "model_name"]
 df <- df[!is.na(df[["RRID"]]), ]
 ## Remove cell lines that map ambiguously.
+## FIXME Drop this: "CVCL_1429" - "CVCL_E995"
 censor <- c(
-    "SIDM00366", # SBC-2
-    "SIDM00400", # SC-1
     "SIDM00054", # PL18
-    "SIDM00122", # BT-549
-    "SIDM00199", # CS1
-    "SIDM00408", # MS-1
-    "SIDM00427", # RH-1
-    "SIDM00911", # COLO-699
-    "SIDM01238", # ST
-    "SIDM01334", # RH-3
-    "SIDM01349", # RH-4
-    "SIDM00126", # K2
-    "SIDM01500", # EW-8,
-    "SIDM01512", # CJM
-    "SIDM01335", # SNU-1272
-    "SIDM01497", # F5
-    "SIDM01507", # DL
-    "SIDM01625" # F-36E
+    "SIDM00400" # SC-1
 )
 df <- df[setdiff(rownames(df), censor), ]
 map[["cmp"]] <- df
