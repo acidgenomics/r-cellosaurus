a918a8b5d14935067259396525a8e0a3507faf65
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-17 10:18:05 -0500

    Rework globals

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -1,8 +1,3 @@
-.pkgName <- packageName()
-.pkgVersion <- packageVersion(.pkgName)
-
-
-
 #' Cellosaurus release version
 #'
 #' @note Updated 2023-01-12.
@@ -14,14 +9,11 @@
 
 
 
-#' pipette test data URL
-#'
-#' @export
-#' @keywords internal
-#' @note Updated 2021-03-16.
-#'
-#' @examples
-#' pipetteTestsURL
+.pkgName <- packageName()
+.pkgVersion <- packageVersion(.pkgName)
+
+
+
 .testsURL <- paste0(
     "https://r.acidgenomics.com/testdata/", tolower(.pkgName), "/",
     "v", .pkgVersion$major, ".", .pkgVersion$minor # nolint
