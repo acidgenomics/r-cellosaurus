34ca182a919250541ab9fd0d76445dcfed97a46f
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-17 16:25:25 -0500

    Rework cello save

--- data-raw/cello.R
+++ data-raw/cello.R
@@ -6,6 +6,7 @@ suppressPackageStartupMessages({
 ## nolint end
 load_all()
 object <- Cellosaurus()
+saveRDS(object, "celloFull.rds")
 object <- object[seq_len(10L), ]
 cello <- object
 use_data(cello, overwrite = TRUE)
