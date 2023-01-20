9166b6594610a7adc17ec208beed1e3a8a1c4b62
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-20 12:45:54 -0500

    Bump to Cellosaurus 44

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -1,11 +1,11 @@
 #' Cellosaurus release version
 #'
-#' @note Updated 2023-01-12.
+#' @note Updated 2023-01-20.
 #' @noRd
 #'
 #' @details
 #' Note that ontologyIndex currently has parsing problems with 44 release.
-.release <- 43L
+.release <- 44L
 
 
 
