caeb3137a4676b772ec61bb3a761d8156f19360e
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-24 16:16:16 -0500

    Improve coverage of checking exported values

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -6,26 +6,30 @@ test_that("export", {
     expect_true(file.exists(x))
     df <- pipette::import(con)
     expect_identical(
-        object = colnames(df),
-        expected = c(
-            "accession",
-            "ageAtSampling",
-            "category",
-            "cellLineName",
-            "depmapId",
-            "isCancer",
-            "isProblematic",
-            "msiStatus",
-            "ncbiTaxonomyId",
-            "ncitDiseaseId",
-            "ncitDiseaseName",
-            "organism",
-            "population",
-            "samplingSite",
-            "sangerModelId",
-            "secondaryAccession",
-            "sexOfCell",
-            "synonyms"
+        object = df[1L, ],
+        expected = data.frame(
+            "accession" = "CVCL_0001",
+            "ageAtSampling" = "30Y",
+            "category" = "Cancer cell line",
+            "cellLineName" = "HEL",
+            "depmapId" = "ACH-000004",
+            "isCancer" = TRUE,
+            "isProblematic" = FALSE,
+            "msiStatus" = paste(
+                "Stable (MSS)",
+                "(PubMed=10739008; PubMed=11226526; Sanger)"
+            ),
+            "ncbiTaxonomyId" = 9606L,
+            "ncitDiseaseId" = "C7152",
+            "ncitDiseaseName" = "Erythroleukemia",
+            "organism" = "Homo sapiens",
+            "population" = "Caucasian",
+            "samplingSite" = "Peripheral blood",
+            "sangerModelId" = "SIDM00594",
+            "secondaryAccession" = NA,
+            "sexOfCell" = "Male",
+            "synonyms" = "Hel, GM06141, GM06141B, Human ErythroLeukemia",
+            row.names = "CVCL_0001"
         )
     )
     AcidBase::unlink2(tempdir)
