f9c9eb9611e6c3bb43a3f27ade3c63121b874d29
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-26 12:56:16 -0500

    Add coverage

--- tests/testthat/test-tnbc.R
+++ tests/testthat/test-tnbc.R
@@ -4,4 +4,10 @@ test_that("tnbc", {
         object = tnbc(object),
         expected = character()
     )
+    object[1L, "comments"][[1L]][["Group"]] <-
+        "Triple negative breast cancer (TNBC) cell line"
+    expect_identical(
+        object = tnbc(object),
+        expected = rownames(object)[[1L]]
+    )
 })
