52294be7277d4b5add4023e523f08cca44922322
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-31 13:12:41 -0500

    Resave examples

--- data-raw/cello.R
+++ data-raw/cello.R
@@ -2,6 +2,7 @@
 suppressPackageStartupMessages({
     library(devtools)
     library(usethis)
+    library(AcidBase)
 })
 ## nolint end
 load_all()
@@ -10,3 +11,27 @@ saveRDS(object, "celloFull.rds")
 object <- object[seq_len(10L), ]
 cello <- object
 use_data(cello, overwrite = TRUE)
+## FIXME Add this to AcidBase package.
+majMinVer <- numeric_version(paste(
+    strsplit(
+        as.character(packageVersion("Cellosaurus")),
+        split = ".",
+        fixed = TRUE
+    )[[1L]][1L:2L],
+    collapse = "."
+))
+shell(
+    command = "aws",
+    args = c(
+        "--profile=acidgenomics",
+        "s3", "cp", "celloFull.rds",
+        pasteURL(
+            "r.acidgenomics.com",
+            "testdata",
+            "cellosaurus",
+            paste0("v", majMinVer),
+            "celloFull.rds"
+            protocol = "s3"
+        )
+    )
+)
