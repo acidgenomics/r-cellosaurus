e3faa3a90e65baf54b16a2f6b79f7ed82c5c7d76
Michael Steinbaugh <mike@steinbaugh.com>
2023-01-31 15:02:53 -0500

    Check TNBC return against full dataset

--- tests/testthat/test-tnbc.R
+++ tests/testthat/test-tnbc.R
@@ -1,3 +1,5 @@
+celloFull <- readRDS(file.path("cache", "celloFull.rds"))
+
 test_that("tnbc", {
     object <- cello
     expect_identical(
@@ -10,4 +12,6 @@ test_that("tnbc", {
         object = tnbc(object),
         expected = rownames(object)[[1L]]
     )
+    object <- celloFull
+    expect_length(tnbc(object), 152L)
 })
