73cb4deb4d359a7c62706406adb1a28b7d1cc7b5
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-23 11:00:38 -0400

    Rework our NCIt to OncoTree mappings approach

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -5,6 +5,7 @@ suppressPackageStartupMessages({
 })
 ## nolint end
 overrides <- import("overrides.csv")
+## Save `R/sysdata.rda` ========================================================
 use_data(
     overrides,
     overwrite = TRUE,
