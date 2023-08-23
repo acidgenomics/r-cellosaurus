1479734f6b790e13090209f80230322a57e30896
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-23 10:35:08 -0400

    Improve show method

--- R/show-methods.R
+++ R/show-methods.R
@@ -2,17 +2,19 @@
 #' @name show
 #' @inherit methods::show params return title
 #' @keywords internal
-#' @note Updated 2022-06-02.
+#' @note Updated 2023-08-23.
 NULL
 
 
 
-## Updated 2022-06-02.
+## Updated 2023-08-23.
 `show,Cellosaurus` <- # nolint
     function(object) {
         showHeader(object)
         showSlotInfo(list(
-            "dataVersion" = metadata(object)[["dataVersion"]]
+            "cells" = nrow(object),
+            "release" = majorVersion(metadata(object)[["dataVersion"]]),
+            "date" = metadata(object)[["date"]]
         ))
     }
 
