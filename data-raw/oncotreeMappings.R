a0b198f74e912d97c78b11e03c084f53ea6f53e9
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-23 11:59:04 -0400

    Draft update to NCIt to OncoTree mapping script

--- data-raw/oncotreeMappings.R
+++ data-raw/oncotreeMappings.R
@@ -1,9 +1,13 @@
+## FIXME Need to rework this.
+
 ## nolint start
 suppressPackageStartupMessages({
     library(devtools)
     library(usethis)
-    library(syntactic)
+    library(goalie)
     library(pipette)
+    library(S4Vectors)
+    library(DepMapAnalysis) # 0.3.0.9000
 })
 ## nolint end
 load_all()
@@ -16,22 +20,62 @@ load_all()
 ##   https://oncotree.mskcc.org/
 
 ## cBioPortal oncotree mappings file.
-url <- "https://raw.githubusercontent.com/cBioPortal/oncotree/master/scripts/ontology_to_ontology_mapping_tool/ontology_mappings.txt"
-
-
-## DepMap 23q2 "Model.csv" file
-## https://figshare.com/ndownloader/files/40448834
-
-
-ontologyMappings <- import(
-    con = system.file(
-        "extdata",
-        "ontologyMappings.tsv",
-        package = .pkgName,
-        mustWork = TRUE
-    )
+df1 <- import(
+    con = pasteURL(
+        "raw.githubusercontent.com",
+        "cBioPortal",
+        "oncotree",
+        "master",
+        "scripts",
+        "ontology_to_ontology_mapping_tool",
+        "ontology_mappings.txt",
+        protocol = "https"
+    ),
+    format = "tsv"
 )
-colnames(ontologyMappings) <- camelCase(colnames(ontologyMappings))
-## Arrange by "oncotreeCode".
-ontologyMappings <-
-    ontologyMappings[order(ontologyMappings[["oncotreeCode"]]), , drop = FALSE]
+df1 <- df1[, c("NCIT_CODE", "ONCOTREE_CODE")]
+colnames(df1) <- c("ncit", "oncotree")
+df1 <- df1[complete.cases(df1), ]
+## FIXME Need to handle weird comma edge case here: C9168,C7967. How can we
+## split this out using base R? Argh...
+df1 <- unique(df1)
+df1 <- df1[order(df1[["ncit"]]), ]
+## This file has NCIt identifiers mapping to multiple OncoTree codes, which is
+## annoying. Drop any of these duplicate cases.
+dupes <- df1[["ncit"]][which(duplicated(df1[["ncit"]]))]
+## > print(dupes)
+##  [1] "C27349" "C27893" "C3058"  "C3768"  "C40090" "C40208" "C40364" "C41247"
+##  [9] "C4221"  "C4290"  "C4519"  "C4906"  "C6344"  "C7151"  "C7724"  "C8106"
+## [17] "C82616" "C9245"  "C9296"  "C9461"
+df1 <- df1[!df1[["ncit"]] %in% dupes, ]
+assert(hasNoDuplicates(df1[["ncit"]]))
+df1[["source"]] <- "cbioportal"
+
+## Can't use the DepMap "Model.csv" file directly, as it doesn't contain any
+## NCIt mappings. It only contains RRID identifiers for Cellosaurus.
+df2 <- DepMapAnalysis:::.importBroadModelInfo()
+## > print(nrow(df2))
+## 1824
+xxx <- DataFrame(
+    "ncit" = df2[["cellosaurus"]][["ncitDiseaseId"]],
+    "oncotree" = df2[["broad"]][["OncotreeCode"]]
+)
+xxx <- xxx[!is.na(xxx[["oncotree"]]), ]
+## Some of these terms are nested: C3798,C3273.
+## How to split these out without using tidyverse?
+
+
+
+map <- rbind(df1, df2)
+map <- map[, c("ncit", "oncotree")]
+map <- unique(map)
+## FIXME Assert that all NCIT codes match expectations.
+## FIXME Assert that all OncoTree codes match expections.
+assert(
+    hasNoDuplicates(map[["ncit"]]),
+    !any(is.na(map[["ncit"]])),
+    !any(is.na(map[["oncotree"]]))
+)
+map <- map[order(map[["ncit"]]), ]
+rownames(map) <- map[["ncit"]]
+
