f849e2d6534cca9c8cfa21a9a41afb09a6f1e5b2
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-23 10:35:08 -0400

    Improve show method

--- R/imports.R
+++ R/imports.R
@@ -27,7 +27,7 @@ NULL
 
 ## Standard functions ==========================================================
 
-#' @importFrom AcidBase pasteURL showHeader showSlotInfo
+#' @importFrom AcidBase majorVersion pasteURL showHeader showSlotInfo
 #' @importFrom AcidCLI abort alert alertInfo
 #' @importFrom IRanges CharacterList IntegerList
 #' @importFrom S4Vectors DataFrame SimpleList
