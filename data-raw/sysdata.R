d406538b930d86a025ed1353577957a4d64f198b
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-24 09:35:12 -0400

    Resave

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -2,12 +2,18 @@
 ## Updated 2023-08-24.
 
 ## nolint start
-library(usethis)
+suppressPackageStartupMessages({
+    library(usethis)
+})
 ## nolint end
 
-overrides <- import("overrides.csv")
+ncit2Oncotree <- readRDS("ncit2Oncotree.rds")
+oncotree <- readRDS("oncotree.rds")
+overrides <- readRDS("overrides.rds")
 
 use_data(
+    ncit2Oncotree,
+    oncotree,
     overrides,
     overwrite = TRUE,
     internal = TRUE
