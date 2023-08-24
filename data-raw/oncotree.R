a6191951bf40b97da0892c1bedfd19316c62e999
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-24 08:33:37 -0400

    Add comments about OncoTree parsing code

--- data-raw/oncotree.R
+++ data-raw/oncotree.R
@@ -1,5 +1,31 @@
-## OncoTree metadata from DepMap.
+## NOTE Alternatively, can check cBioPortal:
+## http://www.cbioportal.org/webservice.do?cmd=getTypesOfCancer
+
+## OncoTree tumor type metadata.
 ## Updated 2023-08-24.
+##
+## See also:
+## - OncoTree API tumor type info:
+##   http://oncotree.mskcc.org/api/tumorTypes
+## - OncoTree API tumor hierarchy info:
+##   http://oncotree.mskcc.org/api/tumorTypes/tree
+## - DepMap 23q2 "Model.csv" file:
+##   https://figshare.com/ndownloader/files/40448834
+## - DepMap uses these column names for OncoTree:
+##   - "OncotreeCode"
+##   - "OncotreeSubtype"
+##   - "OncotreePrimaryDisease"
+##   - "OncotreeLineage"
+## - This script has code that parses the tumor type info:
+##   https://github.com/vanallenlab/moalmanac/blob/main/moalmanac/datasources/
+##     oncotree/get_oncotree.py
+## - This notebook has code that parses the OncoTree hierarchy:
+##   https://github.com/PedroSebe/signature-inference/blob/main/workflow/
+##     notebooks/tidy_oncotree.ipynb
+## - cBioPortal types of cancer
+##   http://www.cbioportal.org/webservice.do?cmd=getTypesOfCancer
+## - This script has code that parses the cBioPortal types of cancer:
+##   https://github.com/ashleyrchen/cancer-web-tool/blob/master/cBioPortal.ipynb
 
 library(syntactic)
 library(pipette)
