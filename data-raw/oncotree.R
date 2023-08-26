3b8047520db88da4965b4cc73c1db477f849eb60
Michael Steinbaugh <mike@steinbaugh.com>
2023-08-26 08:07:14 -0400

    Reformat

--- data-raw/oncotree.R
+++ data-raw/oncotree.R
@@ -3,26 +3,26 @@
 ##
 ## See also:
 ## - OncoTree API tumor type info:
-##   http://oncotree.mskcc.org/api/tumorTypes
+## http://oncotree.mskcc.org/api/tumorTypes
 ## - OncoTree API tumor hierarchy info:
-##   http://oncotree.mskcc.org/api/tumorTypes/tree
+## http://oncotree.mskcc.org/api/tumorTypes/tree
 ## - DepMap 23q2 "Model.csv" file:
-##   https://figshare.com/ndownloader/files/40448834
+## https://figshare.com/ndownloader/files/40448834
 ## - DepMap uses these column names for OncoTree:
-##   - "OncotreeCode"
-##   - "OncotreeSubtype"
-##   - "OncotreePrimaryDisease"
-##   - "OncotreeLineage"
+## - "OncotreeCode"
+## - "OncotreeSubtype"
+## - "OncotreePrimaryDisease"
+## - "OncotreeLineage"
 ## - This script has code that parses the tumor type info:
-##   https://github.com/vanallenlab/moalmanac/blob/main/moalmanac/datasources/
-##     oncotree/get_oncotree.py
+## https://github.com/vanallenlab/moalmanac/blob/main/moalmanac/datasources/
+## oncotree/get_oncotree.py
 ## - This notebook has code that parses the OncoTree hierarchy:
-##   https://github.com/PedroSebe/signature-inference/blob/main/workflow/
-##     notebooks/tidy_oncotree.ipynb
+## https://github.com/PedroSebe/signature-inference/blob/main/workflow/
+## notebooks/tidy_oncotree.ipynb
 ## - cBioPortal types of cancer
-##   http://www.cbioportal.org/webservice.do?cmd=getTypesOfCancer
+## http://www.cbioportal.org/webservice.do?cmd=getTypesOfCancer
 ## - This script has code that parses the cBioPortal types of cancer:
-##   https://github.com/ashleyrchen/cancer-web-tool/blob/master/cBioPortal.ipynb
+## https://github.com/ashleyrchen/cancer-web-tool/blob/master/cBioPortal.ipynb
 
 ## nolint start
 suppressPackageStartupMessages({
