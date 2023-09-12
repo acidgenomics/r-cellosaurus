12a2c4b55453686c92078d97c8116fc5b50f9765
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-12 09:11:32 -0400

    Improve working example

--- R/selectCells-methods.R
+++ R/selectCells-methods.R
@@ -1,4 +1,4 @@
-## FIXME Improve documentation about supported keys (columns).
+## FIXME Allow user to select isCancer, isContaminated, and isProblematic.
 
 
 
@@ -6,18 +6,38 @@
 #' @inherit AcidGenerics::selectCells
 #' @note Updated 2023-09-12.
 #'
+#' @details
+#' Only exact matching is currently supported.
+#'
+#' @section Supported keys:
+#'
+#' - `"category"`
+#' - `"ncbiTaxonomyId"`
+#' - `"ncitDiseaseId"`
+#' - `"ncitDiseaseName"`
+#' - `"oncotreeCode"`
+#' - `"oncotreeMainType"`
+#' - `"oncotreeName"`
+#' - `"oncotreeTissue"`
+#' - `"organism"`
+#' - `"sexOfCell"`
+#'
+#' @return Modified object, subset to only contain matching cell lines, which
+#' are defined in the rows.
+#'
 #' @examples
 #' data(cello)
 #'
 #' ## Cellosaurus ====
 #' object <- cello
-#' dim(object)
-#' lineage <- colData(object)[["lineage"]][[1L]]
-#' print(lineage)
-#' all(colData(object)[["lineage"]] == lineage)
-#' object <- selectCells(object, lineage = lineage)
-#' all(colData(object)[["lineage"]] == lineage)
-#' dim(object)
+#' print(dim(object))
+#' subset <- selectCells(
+#'     object = object,
+#'     category = "Cancer cell line",
+#'     organism = "Homo sapiens",
+#'     sexOfCell = "Female"
+#' )
+#' print(dim(subset))
 NULL
 
 
@@ -35,24 +55,45 @@ NULL
             )),
             msg = "Arguments must be atomic."
         )
-        colData <- colData(object)
-        assert(isSubset(names(args), colnames(colData)))
-        ## FIXME Only allow the user to select factor columns.
-        ## Obtain the cell identifiers.
+        assert(isSubset(
+            x = names(args),
+            y = c(
+                "category",
+                "ncbiTaxonomyId",
+                "ncitDiseaseId",
+                "ncitDiseaseName",
+                "oncotreeCode",
+                "oncotreeMainType",
+                "oncotreeName",
+                "oncotreeTissue",
+                "organism",
+                "sexOfCell"
+            )
+        ))
+        df <- as(object, "DFrame")
+        df <- df[, names(args), drop = FALSE]
+        df <- decode(df)
         list <- Map(
             col = names(args),
             arg = args,
-            MoreArgs = list("data" = colData),
-            f = function(col, arg, data) {
-                rownames(data[data[[col]] %in% arg, , drop = FALSE])
+            MoreArgs = list("df" = df),
+            f = function(col, arg, df) {
+                vals <- df[[col]]
+                if (is(vals, "List")) {
+                    lgl <- any(vals %in% arg)
+                } else {
+                    lgl <- vals %in% arg
+                }
+                which(lgl)
             }
         )
-        cells <- sort(as.character(Reduce(f = intersect, x = list)))
-        assert(hasLength(cells))
-        out <- object[, cells, drop = FALSE]
-        out <- droplevels2(out)
+        i <- sort(Reduce(f = intersect, x = list))
+        assert(
+            hasLength(i),
+            msg = "No cell lines to select."
+        )
+        out <- object[i, , drop = FALSE]
         out
-
     }
 
 
