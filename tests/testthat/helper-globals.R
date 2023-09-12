d29be902b3a87181b63eea43fe9a6c946e1a7c39
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-12 10:03:42 -0400

    Add code coverage

--- tests/testthat/helper-globals.R
+++ tests/testthat/helper-globals.R
@@ -1,4 +1,8 @@
 utils::data(cello, envir = environment())
 
+## nolint start
+hasRows <- goalie::hasRows
+## nolint end
+
 celloFull <- readRDS(file.path("cache", "celloFull.rds"))
 map <- readRDS(file.path("cache", "mapCells.rds"))
