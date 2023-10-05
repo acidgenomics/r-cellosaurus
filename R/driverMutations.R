5d125c09d59bb3d71610ab3d6ca9e13aa62fe8a1
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-05 14:38:01 -0400

    Initial working version of driver mutation extraction

--- R/driverMutations.R
+++ R/driverMutations.R
@@ -3,11 +3,16 @@
 #' @export
 #' @note Updated 2023-10-05.
 #'
+#' @return `CharacterList`.
+#'
 #' @examples
 #' data(cello)
 #' x <- driverMutations(cello)
 #' print(x)
-driverMutations <- function(object) {
+driverMutations <- function(
+        object,
+        format = c("geneName", "hgncId", "both")) {
+    format <- match.arg(format)
     x <- mclapply(
         X = object[["comments"]],
         FUN = function(x) {
@@ -19,6 +24,16 @@ driverMutations <- function(object) {
     names(x) <- rownames(object)
     x <- x[lengths(x) > 0L]
     x <- CharacterList(x)
-    x <- sub(pattern = "Mutation; ", replacement = "", x = x)
+    x <- sub(
+        pattern = "^Mutation; HGNC; ([0-9]+); ([^;]+);.+$",
+        replacement = switch(
+            EXPR = format,
+            "both" = "\\2_\\1",
+            "geneName" = "\\2",
+            "hgncId" = "\\1"
+        ),
+        x = x
+    )
+    x <- unique(x)
     x
 }
---
+++ R/fusions.R
@@ -0,0 +1,14 @@
+#' Gene fusion pairs
+#'
+#' @export
+#' @note Updated 2023-10-05
+#'
+#' @return `CharacterList`.
+#'
+#' @examples
+#' data(cello)
+#' x <- fusions(cello)
+#' print(x)
+fusions <- function(object) {
+    stop("FIXME IN PROGRESS")
+}
