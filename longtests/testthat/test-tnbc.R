962a51942724c2c3bc990fac845236e1f0bf946b
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-06 11:54:22 -0400

    Harden unit tests against specific data version

--- longtests/testthat/test-tnbc.R
+++ longtests/testthat/test-tnbc.R
@@ -1,4 +1,8 @@
 test_that("tnbc", {
     object <- celloFull
+    skip_if_not(identical(
+        x = metadata(object)[["dataVersion"]],
+        y = dataVersion
+    ))
     expect_length(tnbc(object), 161L)
 })
