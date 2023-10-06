c1cac3dd21c198f19fc461ec08eb4087cf8e7428
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-06 05:39:42 -0400

    Ensure we include cell line metadata

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -1,2 +1,17 @@
 .pkgName <- packageName()
 .pkgVersion <- packageVersion(.pkgName)
+
+.minimalCols <- c(
+    "accession",
+    "cellLineName",
+    "atccId",
+    "depmapId",
+    "sangerModelId",
+    "ncitDiseaseId",
+    "ncitDiseaseName",
+    "oncotreeCode",
+    "oncotreeMainType",
+    "oncotreeName",
+    "oncotreeTissue",
+    "organism"
+)
--- R/cellsPerGeneFusion.R
+++ R/cellsPerGeneFusion.R
@@ -1,21 +1,24 @@
 #' Cells per gene fusion
 #'
 #' @export
-#' @note Updated 2023-10-05.
+#' @note Updated 2023-10-06.
 #'
-#' @inheritParams geneFusions
+#' @inheritParams AcidRoxygen::params
 #'
-#' @return `matrix`.
-#' Logical matrix.
+#' @param minCells `integer(1)`.
+#' Minimum number of cells per gene fusion.
+#'
+#' @return `DFrame`.
 #' Gene fusion pairs in columns, cells in rows.
+#' Includes additional cell line metadata on left side.
 #'
 #' @examples
 #' data(cello)
 #'
 #' ## Cellosaurus ====
 #' object <- cello
-#' mat <- cellsPerGeneFusion(object)
-#' print(mat[1L:5L, 1L:5L])
+#' df <- cellsPerGeneFusion(object)
+#' print(df[1L:5L, 1L:5L])
 cellsPerGeneFusion <-
     function(object, minCells = 2L) {
         assert(validObject(object))
@@ -27,10 +30,12 @@ cellsPerGeneFusion <-
         mat <- mat[, keep, drop = FALSE]
         keep <- rowSums(mat) > 0L
         mat <- mat[keep, , drop = FALSE]
-        j <- rownames(mat)
-        rn <- paste0(decode(object[j, "cellLineName"]), " (", j, ")")
-        rownames(mat) <- rn
         i <- order(colSums(mat), decreasing = TRUE)
         mat <- mat[, i, drop = FALSE]
-        mat
+        df1 <- as(mat, "DFrame")
+        j <- rownames(df1)
+        df2 <- as(object, "DFrame")
+        df2 <- df2[j, .minimalCols, drop = FALSE]
+        df <- cbind(df2, df1)
+        df
     }
